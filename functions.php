32cc3b59d8926745fcfc625204171868f0c6ed20
shengoo <shengoo@hotmail.com>
2016-09-07 23:27:01 +0800

    add single for category

--- functions.php
+++ functions.php
@@ -116,3 +116,14 @@ function create_post_type() {
 }
 
 
+//Gets post cat slug and looks for single-[cat slug].php and applies it
+add_filter('single_template', create_function(
+        '$the_template',
+        'foreach( (array) get_the_category() as $cat ) {
+		if ( file_exists(TEMPLATEPATH . "/single-{$cat->slug}.php") )
+		return TEMPLATEPATH . "/single-{$cat->slug}.php"; }
+	return $the_template;' )
+);
+
+
+
--- single-about.php
+++ single-about.php
@@ -3,9 +3,18 @@
  * Created by PhpStorm.
  * User: UC206612
  * Date: 2016/9/1
- * Time: 15:25
+ * Time: 15:26
  */
+get_header();
 
+while ( have_posts() ) : the_post();
+    $format = get_post_format() ? : 'standard';
+    echo $format;;
+    echo var_dump(get_the_category());
+endwhile;
 ?>
 
-<h2><?php the_title(); ?></h2>
+    <h2><?php the_title(); ?></h2>
+<div><?php the_content();?></div>
+
+<?php get_footer(); ?>
